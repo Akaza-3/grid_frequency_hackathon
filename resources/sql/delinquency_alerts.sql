f5ed297456a939cb3df1cbd4f255fb68899538d5
Sahil Kothari <kotharisahil4@gmail.com>
2026-07-23 15:50:09 +0530

    sixty seventh push

--- resources/sql/delinquency_alerts.sql
+++ resources/sql/delinquency_alerts.sql
@@ -1,6 +1,4 @@
 -- resources/sql/delinquency_alerts.sql
---dummy comment
---dummy comment 1
 SELECT *
 FROM `project-ff7c2ef5-8d88-401a-b86.loan_data.loan` l
 JOIN `project-ff7c2ef5-8d88-401a-b86.loan_data.customers` c
